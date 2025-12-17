d6cfd3df6cfdadf2da634b6770f605f2fae9ed88
choneface <gavin.garcia0001@gmail.com>
2025-12-16 22:44:43 -0500

    render proper size

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::rc::Rc;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::Window;
-use winit::dpi::LogicalSize;
+use winit::dpi::PhysicalSize;
 
 #[path = "winit_app.rs"]
 mod winit_app;
@@ -15,7 +15,7 @@ fn main() {
     let event_loop = EventLoop::new().unwrap();
     let context = softbuffer::Context::new(event_loop.owned_display_handle()).unwrap();
     let background = ppm_consumer::read_ppm_file("src/image.ppm");
-    let size = LogicalSize::new(background.width, background.height);
+    let size = PhysicalSize::new(background.width, background.height);
 
     let mut app = winit_app::WinitAppBuilder::with_init(
         |elwt| {
